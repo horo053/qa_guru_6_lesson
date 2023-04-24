7645c212a0240a64ed4f9fe50d3b65545433cf3f
Streshneva Kristina <57588188+horo053@users.noreply.github.com>
2023-04-24 21:02:39 +0300

    Update homework.py

--- tests/homework.py
+++ tests/homework.py
@@ -88,14 +88,17 @@ def print_name_and_argument(func, *args):
 
 def open_browser(browser_name):
     actual_result = print_name_and_argument(open_browser, browser_name)
+    print(actual_result)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = print_name_and_argument(go_to_companyname_homepage, page_url)
+    print(actual_result)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = print_name_and_argument(find_registration_button_on_login_page, page_url, button_text)
+    print(actual_result)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
