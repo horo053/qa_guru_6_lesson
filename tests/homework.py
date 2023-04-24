d002ce1f0236762737394094c252aab7458941eb
Streshneva Kristina <strashneva@proitr.ru>
2023-04-24 18:06:29 +0300

    Создание функции для вывода имени и аргумента функции

--- tests/homework.py
+++ tests/homework.py
@@ -53,12 +53,10 @@ def test_find_suitable_user():
         {"name": "Maria", "age": 18},
     ]
 
-    # Решение найдено на https://www.delftstack.com/ru/howto/python/python-search-list-of-dictionaries/
-
     # TODO найдите пользователя с именем "Olga"
     suitable_users = ([key for key in users if key['name'] == 'Olga'][0])
     assert suitable_users == {"name": "Olga", "age": 45}
-
+    #Решение найдено на https://www.delftstack.com/ru/howto/python/python-search-list-of-dictionaries/
 
     # TODO найдите всех пользователей младше 20 лет
     suitable_users = ([key for key in users if key['age'] < 20])
@@ -84,17 +82,20 @@ def test_readable_function():
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
+# Решение найдено на https://pythonist.ru/strokovye-metody-split-i-join-v-python/
+def print_name_and_argument(func, *args):
+    return f'{func.__name__.replace("_", " ").title()} [{", ".join(args)}]'
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_name_and_argument(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_name_and_argument(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_name_and_argument(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
